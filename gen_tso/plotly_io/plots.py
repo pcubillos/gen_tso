84fb51fa5be968f51b188111b847760e87733b03
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2024-03-24 16:06:01 +0100

    Implemented plotly_sed_spectra() to display SED models.

--- gen_tso/plotly_io/plots.py
+++ gen_tso/plotly_io/plots.py
@@ -17,8 +17,76 @@ def plotly_filters():
     pass
 
 
-def plotly_sed_spectra():
-    pass
+def plotly_sed_spectra(
+        sed_models, labels, highlight_model=None,
+        wl_range=[0.5,12], units='mJy', wl_scale='linear', resolution=250.0,
+    ):
+    """
+    Make a plotly figure of stellar SED spectra.
+    """
+    nmodels = len(sed_models)
+    fig = go.Figure()
+    for j,model in enumerate(sed_models):
+        wl = model['wl']
+        flux = model['flux']
+        if resolution > 0:
+            wl_min = np.amin(wl)
+            wl_max = np.amax(wl)
+            bin_wl = ps.constant_resolution_spectrum(wl_min, wl_max, resolution)
+            bin_flux = ps.bin_spectrum(bin_wl, wl, flux, ignore_gaps=True)
+            mask = np.isfinite(bin_flux)
+            wl = bin_wl[mask]
+            flux = bin_flux[mask]
+
+        if highlight_model is None:
+            linedict = dict(width=1.25)
+            rank = j
+        elif labels[j] == highlight_model:
+            linedict = dict(color='Gold', width=2.0)
+            rank = j + nmodels
+        else:
+            linedict = dict(width=1.25)
+            rank = j
+        fig.add_trace(go.Scatter(
+            x=wl,
+            y=flux,
+            mode='lines',
+            opacity=0.75,
+            name=labels[j],
+            #line=linedict,
+            legendrank=rank,
+        ))
+
+    fig.update_traces(
+        hovertemplate=
+            'wl = %{x:.2f}<br>'+
+            'flux = %{y:.3f}'
+    )
+    fig.update_yaxes(
+        title_text=f'Flux ({units})',
+        title_standoff=0,
+    )
+
+    if wl_scale == 'log':
+        wl_range = [np.log10(wave) for wave in wl_range]
+    fig.update_xaxes(
+        title_text='wavelength (um)',
+        title_standoff=0,
+        range=wl_range,
+        type=wl_scale,
+    )
+
+    fig.update_layout(legend=dict(
+        orientation="h",
+        entrywidth=1.0,
+        entrywidthmode='fraction',
+        yanchor="bottom",
+        xanchor="right",
+        y=1.02,
+        x=1
+    ))
+    fig.update_layout(showlegend=True)
+    return fig
 
 
 def plotly_depth_spectra(
