0c20d60c26a757baad8a3e3feabfb62dc7ec0658
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2024-04-15 21:30:06 +0200

    Refactored entry-point main() function to use the python shiny call instead of subprocess. This allows the gen_tso_app.py to catch the comand line arguments.

--- gen_tso/__main__.py
+++ gen_tso/__main__.py
@@ -1,15 +1,13 @@
 # Copyright (c) 2024 Patricio Cubillos
 # Gen TSO is open-source software under the GPL-2.0 license (see LICENSE)
 
-import subprocess
 import os
+from shiny import run_app
+
 
 def main():
     app = os.path.realpath(os.path.dirname(__file__)) + '/gen_tso_app.py'
-    try:
-        subprocess.call(f'shiny run --reload --launch-browser {app}'.split())
-    except KeyboardInterrupt:
-        pass
+    run_app(app, reload=True, launch_browser=True)
 
 
 if __name__ == "__main__":
