f28331c17f66801a262a00514db106b334cc1aed
pcubillos <patricio.cubillos@oeaw.ac.at>
2024-07-26 17:47:44 +0200

    Implemented --setup_db option for the command line to update exoplanet and pysynphot databases. Some more work could be done (interactive IO).

--- gen_tso/__main__.py
+++ gen_tso/__main__.py
@@ -6,6 +6,7 @@ import sys
 from shiny import run_app
 
 
+
 def main():
     """
     Launch the Gen TSO application.
@@ -13,9 +14,13 @@ def main():
     Usage
     -----
     tso [--debug] [models_folder]
+    tso --setup_db
 
     Optional commands
     -----------------
+    --setup_db:
+        If set, update the NASA exoplanet archive and pysynphot databases
+        Otherwise, launch the TSO app
     --debug:
         If set, run the app with reload=True, which reloads a live
         app if the code is updated.
@@ -23,9 +28,18 @@ def main():
         If set, the app will attempt to load transit, eclipse, and SED
         models from the specified folder.
     """
-    reload = '--debug' in sys.argv
-    app = os.path.realpath(os.path.dirname(__file__)) + '/gen_tso_app.py'
-    run_app(app, reload=reload, launch_browser=True, dev_mode=False)
+    if '--setup_db' in sys.argv:
+        # Import here, otherwise shiny breaks (bc of grequests?)
+        import gen_tso.catalogs as cat
+        from gen_tso.pandeia_io.pandeia_setup import update_synphot_files
+
+        cat.update_exoplanet_archive()
+        status = update_synphot_files()
+
+    else:
+        reload = '--debug' in sys.argv
+        app = os.path.realpath(os.path.dirname(__file__)) + '/gen_tso_app.py'
+        run_app(app, reload=reload, launch_browser=True, dev_mode=False)
 
 
 if __name__ == "__main__":
