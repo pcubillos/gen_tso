ec6aca253d35b6811de9f0bb9b06a96a052d62cc
pcubillos <patricio.cubillos@oeaw.ac.at>
2024-08-02 16:04:39 +0200

    Updated version to 0.5.0

--- gen_tso/version.py
+++ gen_tso/version.py
@@ -1,12 +1,5 @@
 # Copyright (c) 2024 Patricio Cubillos
 # Gen TSO is open-source software under the GPL-2.0 license (see LICENSE)
 
-
-# Pyrat-Bay version:
-TSO_VER = 0  # Major version
-TSO_MIN = 4  # Minor version
-TSO_REV = '0'  # Revision
-
-
-__version__ = f'{TSO_VER}.{TSO_MIN}.{TSO_REV}'
+__version__ = '0.5.0'
 
